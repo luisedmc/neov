704dbdce98e9bac106119be999262a5324199562
Luis Eduardo M. <luiseduardomc019@gmail.com>
2025-01-23 06:37:59 -0300

    repo clean

--- lua/ui/colorshemes/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.setup()
-	require('ui.colorshemes.monored').setup()
-end
-
-return M
--- lua/ui/init.lua
+++
@@ -1,3 +0,0 @@
-require('ui.buf').setup()
-require('ui.stl')
--- require('ui.colorshemes').setup()
